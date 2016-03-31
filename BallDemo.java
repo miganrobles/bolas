bb0255f575d075601a250c134124814a77fcc3e0
miguel <miganrobles@gmail.com>
2016-03-31 11:23:09 +0200

    el método boxBounce crea tantas bolas como el usuario indica

--- BallDemo.java
+++ BallDemo.java
@@ -59,7 +59,7 @@ public class BallDemo
 
         }
     }
-    
+
     /**
      * Cuando se invoque debe dibujar un rectángulo sin relleno en la pantalla y 
      * tantas bolas como se indiquen como parámetro en la invocación del método dentro de dicho rectángulo.
@@ -76,6 +76,30 @@ public class BallDemo
         myCanvas.drawLine(lateralDerecho, ground, lateralDerecho, techo);
         myCanvas.setVisible(true);
         Random aleatorio = new Random();
-     
+        myCanvas.setVisible(true);
+        // draw the ground
+        ArrayList<BoxBall> bolas = new ArrayList<>();
+        
+        for (int i = 0; i < numeroBolas; i++) {
+            float r = aleatorio.nextFloat();
+            float g = aleatorio.nextFloat();
+            float b = aleatorio.nextFloat();
+            Color color = new Color(r, g, b);// Se crea el color de forma aleatoria
+
+            int posicion =  aleatorio.nextInt(150);
+            int radio = aleatorio.nextInt(30);
+            bolas.add(new BoxBall(posicion, posicion, radio, color, ground,lateralDerecho, lateralIzquierdo, techo, myCanvas));
+            bolas.get(i).draw();
+        }
+        
+        boolean finished =  false;
+        while(!finished) {
+            myCanvas.wait(50);           // small delay
+            for (BoxBall bola : bolas) {
+                bola.move();
+            }
+            // stop once ball has travelled a certain distance on x axis
+
+        }
     }
 }
